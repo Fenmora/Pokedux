3fd0fd3d4b828604ec0541d2765f5233cad435d2
fmora <fmora@cmnhospitals.org>
2022-04-11 14:49:54 -0600

    API Integration

--- src/components/PokemonList/PokemonCard.js
+++ src/components/PokemonList/PokemonCard.js
@@ -1,14 +1,30 @@
 import React from "react";
-import { Image, Label } from "semantic-ui-react";
+import { Grid, Icon, Image, Label } from "semantic-ui-react";
 import { MAIN_COLOR, FAV_COLOR } from "../../utils/constants";
+import "./style.css";
+
+/**
+ * *Important information is highlight
+ * ! Do not use
+ * ? Should do this
+ * Todo: refactor this method
+ * @param myparam
+ * */
 
 export const PokemonCard = () => {
   return (
-    <div className="PokemonCard">
-      <Image centered src="" alt="Pokemon Front" />
-      <p>Ditto</p>
-      <Label color={MAIN_COLOR}></Label>
-    </div>
+    <Grid.Column mobile={16} tablet={8} computer={4}>
+      <div className="PokemonCard">
+        <Icon name="favorite" color={FAV_COLOR}></Icon>
+        <Image
+          centered
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
+          alt="Pokemon Front"
+        />
+        <p className="Pokemon-Title">Ditto</p>
+        <Label color={MAIN_COLOR}>Normal</Label>
+      </div>
+    </Grid.Column>
   );
 };
 export default PokemonCard;
