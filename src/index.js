0f3097ca2eec476829e6136321e38a704595cad3
fmora <fmora@cmnhospitals.org>
2022-04-17 20:56:18 -0600

    creating middlewares

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./containers/App";
-import { createStore } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import pokemonReducer from "./reducers/pokemonReducer";
 import "./index.css";
-const store = createStore(
-  pokemonReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+import { logActions } from "./middlewares";
+
+const composedEnhacers = compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(logActions)
 );
+
+const store = createStore(pokemonReducer, composedEnhacers);
 
 ReactDOM.render(
   <Provider store={store}>
