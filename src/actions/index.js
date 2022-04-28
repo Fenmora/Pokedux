77568c6939214ed96954b1ff17e2ad8d85f2b508
Fender J. Mora <fenmora@outlook.es>
2022-04-27 21:08:26 -0600

    Revert "Raco dev"

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,17 @@
 import {
-  SET_POKEMON,
+  SET_POKEMONS,
   SET_ERROR,
   CLEAR_ERROR,
-  TOGGLE_LOADER,
-  SET_FAVORITE,
-} from "./type";
-import { getPokemons, getPokemonsWithDetails } from "../api/getPokemons";
+  FETCH_POKEMON_DETAIL,
+} from './types';
 
-export const setPokemon = (payload) => ({
-  type: SET_POKEMON,
+export const setPokemons = (payload) => ({
+  type: SET_POKEMONS,
+  payload,
+});
+
+export const fetchPokemonDetails = (payload) => ({
+  type: FETCH_POKEMON_DETAIL,
   payload,
 });
 
@@ -21,42 +24,3 @@ export const clearError = (payload) => ({
   type: CLEAR_ERROR,
   payload,
 });
-
-export const toggleLoader = () => ({
-  type: TOGGLE_LOADER,
-  // payload,
-});
-
-export const setFavorite = (payload) => ({
-  type: SET_FAVORITE,
-  payload,
-});
-
-// export const getPokemonWithDetails = () => (dispatch) => {
-//   getPokemons()
-//     .then((res) => {
-//       const pokemonList = res.results;
-//       return Promise.all(pokemonList.map((pokemon) => axios.get(pokemon.url)));
-//     })
-//     .then((pokemonResponses) => {
-//       const pokemonsData = pokemonResponses.map((response) => response.data);
-//       dispatch(setPokemon(pokemonsData));
-//     })
-//     .catch((error) => {
-//       dispatch(setError({ message: "Ocurrió un error", error }));
-//     });
-// };
-
-export const fetchPokemonsWithDetails = () => async (dispatch) => {
-  try {
-    dispatch(toggleLoader());
-    const response = await getPokemons();
-    const pokemons = response.results;
-    const pokemonsWithDetails = await getPokemonsWithDetails(pokemons);
-    dispatch(setPokemon(pokemonsWithDetails));
-    dispatch(toggleLoader());
-  } catch (error) {
-    dispatch(setError({ message: "Oops! Something went wrong.", error }));
-    dispatch(toggleLoader());
-  }
-};
